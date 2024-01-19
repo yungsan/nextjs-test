22d743df02c8435b8fcf6836ca26d9535b969a22
Dung Nguyen <dunguyen1608@gmail.com>
2024-01-19 18:04:48 +0700

    first nextjs project

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import '@/app/ui/global.css';
+import { inter, lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        {children}
+      </body>
+    </html>
+  );
+}
